33911185218082a28ee233f9344f845ff15d56ae
harshit kumar <hsharma64143@gmail.com>
2026-06-19 16:04:56 +0530

    this is prompt related file

---
+++ prompt.py
@@ -0,0 +1,9 @@
+text=input(" enter the text")
+
+# Encoding (reverse the text)
+encoded = text[::-1]
+print("Encoded:", encoded)
+
+# Decoding (reverse again)
+decoded = encoded[::-1]
+print("Decoded:", decoded)
